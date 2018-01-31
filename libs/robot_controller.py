6d9339a98cfcff1130f23af9cf93c47e3579271e
doyleke <doyleke@rose-hulman.edu>
2018-01-31 16:09:43 -0500

    doyleke- we made the timed drive in rachels sandbox doyleke- we started init in robot controller

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -20,8 +20,8 @@ class Snatch3r(object):
     """Commands for the Snatch3r robot that might be useful in many different programs."""
 
     def __init__(self):
-        self.left_motor =
-        self.right_motor =
+        self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+        self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
 
         assert self.left_motor.connected
         assert self.right_motor.connected
