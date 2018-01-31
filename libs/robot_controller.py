c88a1876381166a54555e2d47f71810a96434bc9
doyleke <doyleke@rose-hulman.edu>
2018-01-31 15:56:09 -0500

    doyleke- we made the timed drive in rachels sandbox doyleke- we started init in robot controller

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -18,6 +18,13 @@ import time
 
 class Snatch3r(object):
     """Commands for the Snatch3r robot that might be useful in many different programs."""
+
+    def __init__(self):
+        self.left_motor =
+        self.right_motor =
+
+        assert self.left_motor.connected
+        assert self.right_motor.connected
     
     # TODO: Implement the Snatch3r class as needed when working the sandox exercises
     # (and delete these comments)
