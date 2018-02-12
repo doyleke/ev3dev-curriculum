6f8f1e852faa787f2dfb21938a60eb9b131b062f
petersa <petersa@rose-hulman.edu>
2018-02-12 16:39:18 -0500

    petersa on doyleke follow a line

--- sandbox/doyleke/analog_sensors/color_sensor/m2_follow_a_line.py
+++ sandbox/doyleke/analog_sensors/color_sensor/m2_follow_a_line.py
@@ -84,18 +84,13 @@ def follow_the_line(robot, white_level, black_level):
     # Optional extra - For a harder challenge could you drive on the black line and handle left or right turns?
 
     while not robot.touch_sensor.is_pressed:
-        if robot.color_sensor.reflected_light_intensity != black_level:
-            robot.turn_right(300)
-            time.sleep(.02)
-            #break
-            #if robot.color_sensor.reflected_light_intensity == white_level:
-                #robot.turn_left(600)
-                #time.sleep(.02)
-        robot.drive_inches(1, 300)
-        time.sleep(.02)
-        # if robot.color_sensor.reflected_light_intensity != black_level:
-            # robot.turn_right(300)
-            # time.sleep(0.01)
+        if robot.color_sensor.reflected_light_intensity <= black_level:
+            robot.motor_run(300, 300)
+            time.sleep(0.1)
+        if robot.color_sensor.reflected_light_intensity >= white_level:
+            robot.motor_run(500, 200)
+            time.sleep(0.1)
+
     robot.stop_motors()
     ev3.Sound.speak("Done")
 
