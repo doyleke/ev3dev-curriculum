ddc77dbc8447b59c6da025a3f52e7a35f0548ffc
petersa <petersa@rose-hulman.edu>
2018-02-12 16:15:51 -0500

    petersa on doyleke follow a line

--- sandbox/doyleke/analog_sensors/color_sensor/m2_follow_a_line.py
+++ sandbox/doyleke/analog_sensors/color_sensor/m2_follow_a_line.py
@@ -85,12 +85,12 @@ def follow_the_line(robot, white_level, black_level):
 
     while not robot.touch_sensor.is_pressed:
         if robot.color_sensor.reflected_light_intensity != black_level:
-            robot.turn_left(300)
+            robot.turn_left(600)
             time.sleep(.01)
             if robot.color_sensor.reflected_light_intensity == white_level:
                 robot.turn_right(600)
                 time.sleep(.01)
-        robot.motor_run(600, 600)
+        robot.motor_run(500, 500)
         # if robot.color_sensor.reflected_light_intensity != black_level:
             # robot.turn_right(300)
             # time.sleep(0.01)
