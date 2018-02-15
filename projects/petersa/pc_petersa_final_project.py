8a86ea85981571f19024bbe68bd26d202f323df8
petersa <petersa@rose-hulman.edu>
2018-02-15 15:15:48 -0500

    petersa in pc_petersa_final_project added a GUI

--- projects/petersa/pc_petersa_final_project.py
+++ projects/petersa/pc_petersa_final_project.py
@@ -12,10 +12,10 @@ def main():
     controller = ttk.Frame()
     controller.grid()
 
-    part = ttk.Label(root, text="R-MOTOR, L-MOTOR, ")
-    left_speed_label.grid(row=0, column=0)
-    left_speed_entry = ttk.Entry(main_frame, width=8)
-    left_speed_entry.insert(0, "600")
+    part = ttk.Label(root, text="R-MOTOR, L-MOTOR, ARM")
+    part.grid(row=0, column=0)
+    part_entry = ttk.Entry(root, width=10)
+    part_entry.insert(0, "600")
     left_speed_entry.grid(row=1, column=0)
 
     root.mainloop()
