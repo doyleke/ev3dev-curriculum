db549eec483c34758f5266b2bd16fb1e100a50c9
petersa <petersa@rose-hulman.edu>
2018-02-15 15:13:33 -0500

    petersa in pc_petersa_final_project added a GUI

--- projects/petersa/pc_petersa_final_project.py
+++ projects/petersa/pc_petersa_final_project.py
@@ -4,3 +4,21 @@ import tkinter
 from tkinter import ttk
 
 import mqtt_remote_method_calls as com
+
+
+def main():
+    root = tkinter.Tk()
+    root.title('Robot Twister')
+    controller = ttk.Frame()
+    controller.grid()
+
+    part = ttk.Label(root, text="R-MOTOR, L-MOTOR, ")
+    left_speed_label.grid(row=0, column=0)
+    left_speed_entry = ttk.Entry(main_frame, width=8)
+    left_speed_entry.insert(0, "600")
+    left_speed_entry.grid(row=1, column=0)
+
+    root.mainloop()
+
+
+main()
