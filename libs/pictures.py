632109318d8f3fc171f3c2e5ff0e6ea0cfd3dc4f
doyleke <doyleke@rose-hulman.edu>
2018-02-20 00:13:54 -0500

    doyleke project update

---
+++ libs/pictures.py
@@ -0,0 +1,6 @@
+""" doyleke
+I am uploading images for use on the brickman screen
+I downloaded these images from
+https://sites.google.com/site/ev3basic/ev3-basic-
+programming/using-buttons-the-screen-and-the-leds/lego-ev3-standard-bitmaps
+"""
